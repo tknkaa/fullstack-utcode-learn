9dda78a6f700d7028c60bbcf2874cfd991832055
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-01-03 19:49:23 +0900

    chore: initialize git, create package.json, specify type as module and npm workspaces

---
+++ devshell.nix
@@ -0,0 +1,14 @@
+{ pkgs }:
+pkgs.mkShell {
+  # Add build dependencies
+  packages = with pkgs;[
+    nodejs_24 ];
+
+  # Add environment variables
+  env = { };
+
+  # Load custom bash code
+  shellHook = ''
+
+  '';
+}
