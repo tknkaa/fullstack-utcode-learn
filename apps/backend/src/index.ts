8f6fd44c69b3feb19dba21b668f38ffa58ff36c4
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-01-03 22:06:48 +0900

    chore: format with biome

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,29 +1,29 @@
+import cors from "cors";
 import e from "express";
-import cors from "cors"
-import { usersTable } from "./db/schema";
 import { db } from "./db";
+import { usersTable } from "./db/schema";
 
-const app = e()
+const app = e();
 
-app.use(cors())
-app.use(e.json())
+app.use(cors());
+app.use(e.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello, world!")
-})
+	res.send("Hello, world!");
+});
 
 app.post("/users", async (req, res) => {
-    const { name, age, email } = req.body
-    const user: typeof usersTable.$inferInsert = {
-        name,
-        age,
-        email,
-    };
-    await db.insert(usersTable).values(user);
-    const users = await db.select().from(usersTable);
-    return res.json(users)
-})
+	const { name, age, email } = req.body;
+	const user: typeof usersTable.$inferInsert = {
+		name,
+		age,
+		email,
+	};
+	await db.insert(usersTable).values(user);
+	const users = await db.select().from(usersTable);
+	return res.json(users);
+});
 
 app.listen(3000, () => {
-    console.log("express server is running")
-})
+	console.log("express server is running");
+});
--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,37 +1,55 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function App() {
-  const [name, setName] = useState("")
-  const [age, setAge] = useState(0)
-  const [email, setEmail] = useState("")
-  const sayHello = async () => {
-    const res = await fetch("http://localhost:3000/")
-    const text = await res.text()
-    alert(text)
-  }
-  const createUser = async () => {
-    const res = await fetch("http://localhost:3000/users", {
-      "method": "POST",
-      "headers": { "Content-Type": "application/json" },
-      "body": JSON.stringify({
-        name,
-        age,
-        email
-      })
-    })
-    const users = await res.json()
-    setEmail("")
-    setAge(0)
-    setEmail("")
-    console.log(users)
-  }
-  return (
-    <>
-      <button onClick={sayHello}>say hello</button>
-      <input placeholder="name" onChange={(e) => { setName(e.target.value) }} type="text" />
-      <input placeholder="age" onChange={(e) => { setAge(Number(e.target.value)) }} type="number" />
-      <input placeholder="email" onChange={(e) => {setEmail(e.target.value) }} type="email" />
-      <button onClick={createUser}>create user</button>
-    </>
-  )
+	const [name, setName] = useState("");
+	const [age, setAge] = useState(0);
+	const [email, setEmail] = useState("");
+	const sayHello = async () => {
+		const res = await fetch("http://localhost:3000/");
+		const text = await res.text();
+		alert(text);
+	};
+	const createUser = async () => {
+		const res = await fetch("http://localhost:3000/users", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				name,
+				age,
+				email,
+			}),
+		});
+		const users = await res.json();
+		setEmail("");
+		setAge(0);
+		setEmail("");
+		console.log(users);
+	};
+	return (
+		<>
+			<button onClick={sayHello}>say hello</button>
+			<input
+				placeholder="name"
+				onChange={(e) => {
+					setName(e.target.value);
+				}}
+				type="text"
+			/>
+			<input
+				placeholder="age"
+				onChange={(e) => {
+					setAge(Number(e.target.value));
+				}}
+				type="number"
+			/>
+			<input
+				placeholder="email"
+				onChange={(e) => {
+					setEmail(e.target.value);
+				}}
+				type="email"
+			/>
+			<button onClick={createUser}>create user</button>
+		</>
+	);
 }
