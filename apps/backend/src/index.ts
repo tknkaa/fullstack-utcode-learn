54d03aaa9786308a06a6b39ff063efe92c4cc00b
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-01-03 21:20:58 +0900

    feat: create api endpoint to create user

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,12 +1,27 @@
 import e from "express";
 import cors from "cors"
+import { usersTable } from "./db/schema";
+import { db } from "./db";
 
 const app = e()
 
 app.use(cors())
+app.use(e.json())
 
 app.get("/", (req, res) => {
     res.send("Hello, world!")
+})
+
+app.post("/users", async (req, res) => {
+    const { name, age, email } = req.body
+    const user: typeof usersTable.$inferInsert = {
+        name,
+        age,
+        email,
+    };
+    await db.insert(usersTable).values(user);
+    const users = await db.select().from(usersTable);
+    return res.json(users)
 })
 
 app.listen(3000, () => {
