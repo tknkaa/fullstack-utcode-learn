2b58f16bf8cc191362793cf2f4255b3e3f9c6769
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-01-03 22:00:21 +0900

    feat: create form to create user

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,12 +1,37 @@
+import { useState } from "react"
+
 export default function App() {
-  const handleClick = async () => {
+  const [name, setName] = useState("")
+  const [age, setAge] = useState(0)
+  const [email, setEmail] = useState("")
+  const sayHello = async () => {
     const res = await fetch("http://localhost:3000/")
     const text = await res.text()
     alert(text)
   }
+  const createUser = async () => {
+    const res = await fetch("http://localhost:3000/users", {
+      "method": "POST",
+      "headers": { "Content-Type": "application/json" },
+      "body": JSON.stringify({
+        name,
+        age,
+        email
+      })
+    })
+    const users = await res.json()
+    setEmail("")
+    setAge(0)
+    setEmail("")
+    console.log(users)
+  }
   return (
     <>
-      <button onClick={handleClick}>say hello</button>
+      <button onClick={sayHello}>say hello</button>
+      <input placeholder="name" onChange={(e) => { setName(e.target.value) }} type="text" />
+      <input placeholder="age" onChange={(e) => { setAge(Number(e.target.value)) }} type="number" />
+      <input placeholder="email" onChange={(e) => {setEmail(e.target.value) }} type="email" />
+      <button onClick={createUser}>create user</button>
     </>
   )
 }
